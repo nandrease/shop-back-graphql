9bec6043c5200b0c82cf7977608f8c9ea50553b1
nandrease <neeme.andreas.eller@gmail.com>
2019-09-06 12:16:46 +0300

    Simple mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        global.dogs = global.dogs || []
+        //create a dog
+        const newDog = { name: args.name }
+        global.dogs.push(newDog)
+        console.log('create a dog', global.dogs)
+        return newDog
+    }
+};
 
 module.exports = Mutations;
